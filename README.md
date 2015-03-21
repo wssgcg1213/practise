b6125bcad7d813a877cf2065e3cf17f1d42fe847
Liuchenling <wssgcg1213@qq.com>
2015-03-21 23:10:35 +0800

    finish & fix bugs

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # 一些练习
 ---
 
-2015-03-19
+2015-03-19 created
+
+1. mobileDemo
+2. layoutDemo
+3. ajaxSeries
+4. lingSlider
+5. arrayQuestion刘晨凌 重庆邮电大学 计算机科学与技术 前端开发工程师(实习)
--- ajaxSeries/ajaxSeries.demo.js
+++ ajaxSeries/ajaxSeries.demo.js
@@ -2,6 +2,16 @@
  * Created by Liuchenling on 3/19/15.
  */
 $(document).ready(function(){
-    var a = $('#demoForm').ajaxSeries({});
-    console.log(a);
+    var ret = $('#demoForm').ajaxSeries({
+        //这个对象的参数会传入$.ajax函数的参数执行
+        url: "http://the.domain.here/path?query",
+        dataType: "json",
+        success: function(res){
+            //...
+        },
+        complete: function(res){
+            //...
+        }
+    });
+    console.log(ret);
 });
